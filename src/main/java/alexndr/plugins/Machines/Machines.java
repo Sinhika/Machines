d80b2b601466aa985e8aec98d69639ebf3c71785
Sinhika <sinhika@republicofnewhome.org>
2016-12-02 08:41:13 -0600

    renamed resources to have lower-case names

--- src/main/java/alexndr/plugins/Machines/Machines.java
+++ src/main/java/alexndr/plugins/Machines/Machines.java
@@ -1,15 +1,6 @@
 package alexndr.plugins.Machines;
 
-import alexndr.api.core.LogHelper;
-import alexndr.api.core.UpdateChecker;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies="required-after:simplecore")
 public class Machines {
@@ -24,7 +15,6 @@ public class Machines {
 		//Configuration
 		ModInfo.setModInfoProperties(event);
 		Settings.createOrLoadSettings(event);
-		if(Settings.updateChecker.asBoolean()) {UpdateChecker updateChecker = new UpdateChecker(ModInfo.ID, ModInfo.VERSION, ModInfo.VERSIONURL);}
 		
 		//Content
 		Content.preInitialize();
