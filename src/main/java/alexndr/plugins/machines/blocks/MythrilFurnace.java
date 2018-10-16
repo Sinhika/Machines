11e128eb27db660b69c7f106ebb0dce4f82af9d1
Sinhika <sinhika@republicofnewhome.org>
2018-10-15 19:04:28 -0500

    fixed mining furnace bug, updated update.json

--- src/main/java/alexndr/plugins/machines/blocks/MythrilFurnace.java
+++ src/main/java/alexndr/plugins/machines/blocks/MythrilFurnace.java
@@ -1,5 +1,7 @@
 package alexndr.plugins.machines.blocks;
 
+import java.util.Random;
+
 import alexndr.api.content.blocks.SimpleFurnace;
 import alexndr.api.registry.ContentCategories;
 import alexndr.plugins.machines.Machines;
@@ -10,6 +12,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.stats.StatList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -30,7 +33,16 @@ public class MythrilFurnace extends SimpleFurnace<MythrilFurnaceTileEntity>
         super(furnace_name, Machines.plugin, Material.IRON, ContentCategories.Block.MACHINE, is_active);
     } // end ctor
 
-    @Override
+    /* (non-Javadoc)
+	 * @see alexndr.api.content.blocks.SimpleFurnace#getItemDropped(net.minecraft.block.state.IBlockState, java.util.Random, int)
+	 */
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) 
+	{
+		return Item.getItemFromBlock(MythrilFurnace.unlit_furnace);
+	}
+
+	@Override
     public void setAdditionalProperties()
     {
         super.setAdditionalProperties();
