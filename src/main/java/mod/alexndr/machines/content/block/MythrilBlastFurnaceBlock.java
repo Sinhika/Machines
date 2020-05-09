ea991f46c92e53987d43c21df29e50254f2dfbf0
Cynthia Higginbotham <cynthia.c.higginboth.ctr@navy.mil>
2020-05-09 07:24:53 -0500

    refactored smoker & blast furnace blocks to have abstract super classes, because they all use the same animateTick(), but it's different from furnace

--- src/main/java/mod/alexndr/machines/content/block/MythrilBlastFurnaceBlock.java
+++ src/main/java/mod/alexndr/machines/content/block/MythrilBlastFurnaceBlock.java
@@ -1,24 +1,18 @@
 package mod.alexndr.machines.content.block;
 
-import java.util.Random;
-
 import javax.annotation.Nullable;
 
-import mod.alexndr.machines.api.content.AbstractModFurnaceBlock;
+import mod.alexndr.machines.api.content.AbstractModBlastFurnaceBlock;
 import mod.alexndr.machines.content.tile.MythrilBlastFurnaceTileEntity;
 import mod.alexndr.machines.init.ModTileEntityTypes;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.stats.Stats;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
@@ -26,7 +20,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class MythrilBlastFurnaceBlock extends AbstractModFurnaceBlock
+public class MythrilBlastFurnaceBlock extends AbstractModBlastFurnaceBlock
 {
 
     public MythrilBlastFurnaceBlock(Properties builder)
@@ -71,26 +65,5 @@ public class MythrilBlastFurnaceBlock extends AbstractModFurnaceBlock
         }
         return ActionResultType.SUCCESS;
     }
-
-    @Override
-    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand)
-    {
-        if (stateIn.get(BURNING)) {
-            double d0 = (double)pos.getX() + 0.5D;
-            double d1 = (double)pos.getY();
-            double d2 = (double)pos.getZ() + 0.5D;
-            if (rand.nextDouble() < 0.1D) {
-               worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_BLASTFURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
-            }
-
-            Direction direction = stateIn.get(HORIZONTAL_FACING);
-            Direction.Axis direction$axis = direction.getAxis();
-            double d4 = rand.nextDouble() * 0.6D - 0.3D;
-            double d5 = direction$axis == Direction.Axis.X ? (double)direction.getXOffset() * 0.52D : d4;
-            double d6 = rand.nextDouble() * 9.0D / 16.0D;
-            double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getZOffset() * 0.52D : d4;
-            worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
-         }
-    } // end animateTick()
 
 } // end-class
--- src/main/java/mod/alexndr/machines/content/block/MythrilSmokerBlock.java
+++ src/main/java/mod/alexndr/machines/content/block/MythrilSmokerBlock.java
@@ -1,31 +1,24 @@
 package mod.alexndr.machines.content.block;
 
-import java.util.Random;
-
-import mod.alexndr.machines.api.content.AbstractModFurnaceBlock;
+import mod.alexndr.machines.api.content.AbstractModSmokerBlock;
 import mod.alexndr.machines.content.tile.MythrilSmokerTileEntity;
 import mod.alexndr.machines.init.ModTileEntityTypes;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.stats.Stats;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.ItemStackHandler;
 
-public class MythrilSmokerBlock extends AbstractModFurnaceBlock
+public class MythrilSmokerBlock extends AbstractModSmokerBlock
 {
 
     public MythrilSmokerBlock(Properties builder)
@@ -84,21 +77,5 @@ public class MythrilSmokerBlock extends AbstractModFurnaceBlock
         }
         return ActionResultType.SUCCESS;
     }
-
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand)
-    {
-        if (stateIn.get(BURNING)) {
-            double d0 = (double)pos.getX() + 0.5D;
-            double d1 = (double)pos.getY();
-            double d2 = (double)pos.getZ() + 0.5D;
-            if (rand.nextDouble() < 0.1D) {
-               worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_SMOKER_SMOKE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
-            }
-
-            worldIn.addParticle(ParticleTypes.SMOKE, d0, d1 + 1.1D, d2, 0.0D, 0.0D, 0.0D);
-         }
-     } // animateTick()
 
 } // end class
