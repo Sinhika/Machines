4ee6d3f8424e79fbf414a940f0415204f07c937f
Sinhika <sinhika@republicofnewhome.org>
2020-05-09 07:24:53 -0500

    refactored smoker & blast furnace blocks to have abstract super classes, because they all use the same animateTick(), but it's different from furnace

--- src/main/java/mod/alexndr/machines/config/ServerConfig.java
+++ src/main/java/mod/alexndr/machines/config/ServerConfig.java
@@ -20,15 +20,15 @@ final class ServerConfig {
 	{
 		builder.push("general");
 		mythrilFurnaceFuelMultiplier = builder
-		     .comment("Multiplier for fuel duration in mythril furnace")
+		     .comment("Multiplier for fuel duration in mythril furnaces")
 		    .translation(Machines.MODID + "config.mythrilFurnaceFuelMultiplier")
 		    .defineInRange("mythrilFurnaceFuelMultiplier", 2.01, 1.00, 99.9);
 		onyxFurnaceYieldChance = builder
-		    .comment("Per cent (%) chance that onyx furnace will yield extra output")
+		    .comment("Per cent (%) chance that onyx furnaces will yield extra output")
 		    .translation(Machines.MODID + "config.onyxFurnaceYieldChance")
 		     .defineInRange("onyxFurnaceYieldChance", 33, 0, 100);
 		onyxFurnaceYieldAmount = builder
-		    .comment("Number of additional items yielded by onyx furnace")
+		    .comment("Number of additional items yielded by onyx furnaces")
 		    .translation(Machines.MODID + "config.onyxFurnaceYieldAmount")
             .defineInRange("onyxFurnaceYieldAmount", 1, 0, 64);
 		builder.pop();
