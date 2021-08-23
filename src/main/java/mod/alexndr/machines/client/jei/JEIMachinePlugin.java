ab799c9ed2ba0f889e87de4a1bdb9c97e04b4b06
Sinhika <sinhika@republicofnewhome.org>
2021-08-23 15:32:40 -0500

    added JEI support back in; implemented JEI transfer handlers and click areas

--- src/main/java/mod/alexndr/machines/client/jei/JEIMachinePlugin.java
+++ src/main/java/mod/alexndr/machines/client/jei/JEIMachinePlugin.java
@@ -3,11 +3,25 @@ package mod.alexndr.machines.client.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import mod.alexndr.machines.Machines;
+import mod.alexndr.machines.client.gui.MythrilBlastFurnaceScreen;
+import mod.alexndr.machines.client.gui.MythrilFurnaceScreen;
+import mod.alexndr.machines.client.gui.MythrilSmokerScreen;
+import mod.alexndr.machines.client.gui.OnyxBlastFurnaceScreen;
+import mod.alexndr.machines.client.gui.OnyxFurnaceScreen;
+import mod.alexndr.machines.client.gui.OnyxSmokerScreen;
+import mod.alexndr.machines.content.container.MythrilBlastFurnaceContainer;
+import mod.alexndr.machines.content.container.MythrilFurnaceContainer;
+import mod.alexndr.machines.content.container.MythrilSmokerContainer;
+import mod.alexndr.machines.content.container.OnyxBlastFurnaceContainer;
+import mod.alexndr.machines.content.container.OnyxFurnaceContainer;
+import mod.alexndr.machines.content.container.OnyxSmokerContainer;
 import mod.alexndr.machines.init.ModBlocks;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 @JeiPlugin
 public class JEIMachinePlugin implements IModPlugin 
@@ -23,21 +37,52 @@ public class JEIMachinePlugin implements IModPlugin
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registry)
     {
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.mythril_furnace.get()), 
-                VanillaRecipeCategoryUid.FURNACE);
+                VanillaRecipeCategoryUid.FURNACE, VanillaRecipeCategoryUid.FUEL);
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.mythril_blast_furnace.get()), 
-                VanillaRecipeCategoryUid.BLASTING);
+                VanillaRecipeCategoryUid.BLASTING, VanillaRecipeCategoryUid.FUEL);
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.mythril_smoker.get()), 
-                VanillaRecipeCategoryUid.SMOKING);
+                VanillaRecipeCategoryUid.SMOKING, VanillaRecipeCategoryUid.FUEL);
         
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.onyx_furnace.get()), 
-                VanillaRecipeCategoryUid.FURNACE);
+                VanillaRecipeCategoryUid.FURNACE, VanillaRecipeCategoryUid.FUEL);
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.onyx_blast_furnace.get()), 
-                VanillaRecipeCategoryUid.BLASTING);
+                VanillaRecipeCategoryUid.BLASTING, VanillaRecipeCategoryUid.FUEL);
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.onyx_smoker.get()), 
-                VanillaRecipeCategoryUid.SMOKING);
+                VanillaRecipeCategoryUid.SMOKING, VanillaRecipeCategoryUid.FUEL);
     }
 
+    
     @Override
+	public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration)
+	{
+		registration.addRecipeTransferHandler(MythrilFurnaceContainer.class, VanillaRecipeCategoryUid.FURNACE, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(MythrilFurnaceContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+		registration.addRecipeTransferHandler(MythrilSmokerContainer.class, VanillaRecipeCategoryUid.SMOKING, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(MythrilSmokerContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+		registration.addRecipeTransferHandler(MythrilBlastFurnaceContainer.class, VanillaRecipeCategoryUid.BLASTING, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(MythrilBlastFurnaceContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxFurnaceContainer.class, VanillaRecipeCategoryUid.FURNACE, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxFurnaceContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxSmokerContainer.class, VanillaRecipeCategoryUid.SMOKING, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxSmokerContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxBlastFurnaceContainer.class, VanillaRecipeCategoryUid.BLASTING, 0, 1, 3, 36);
+		registration.addRecipeTransferHandler(OnyxBlastFurnaceContainer.class, VanillaRecipeCategoryUid.FUEL, 1, 1, 3, 36);
+	}
+
+
+	@Override
+	public void registerGuiHandlers(IGuiHandlerRegistration registration)
+	{
+		registration.addRecipeClickArea(MythrilFurnaceScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.FURNACE, VanillaRecipeCategoryUid.FUEL);
+		registration.addRecipeClickArea(MythrilSmokerScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.SMOKING, VanillaRecipeCategoryUid.FUEL);
+		registration.addRecipeClickArea(MythrilBlastFurnaceScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.BLASTING, VanillaRecipeCategoryUid.FUEL);
+		registration.addRecipeClickArea(OnyxFurnaceScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.FURNACE, VanillaRecipeCategoryUid.FUEL);
+		registration.addRecipeClickArea(OnyxSmokerScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.SMOKING, VanillaRecipeCategoryUid.FUEL);
+		registration.addRecipeClickArea(OnyxBlastFurnaceScreen.class, 78, 32, 28, 23, VanillaRecipeCategoryUid.BLASTING, VanillaRecipeCategoryUid.FUEL);
+	}
+
+
+	@Override
     public ResourceLocation getPluginUid()
     {
         return ID;
